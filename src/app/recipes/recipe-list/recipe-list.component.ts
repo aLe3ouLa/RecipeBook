8f03f1777bebd4d6e6b6d9314b23d8ee82b03bc1
abarka@ubitech.eu <abarka@ubitech.eu>
2019-04-18 15:35:09 +0300

    Add router for recipe details components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,10 +13,16 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private _recipeServices: RecipeService) { }
+  constructor(private _recipeServices: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this._recipeServices.getRecipes();
+  }
+
+  onNewRecipe() {
+  this.router.navigate(["new"], {relativeTo: this.route});
   }
 
 }
