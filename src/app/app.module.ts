f7b0bb4f9a09b342a3699a3cae9067c7eb51c6a6
abarka@ubitech.eu <abarka@ubitech.eu>
2019-05-06 14:08:36 +0300

    Add ngForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -19,6 +20,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 @NgModule({
   declarations: [
     AppComponent,
+    FormsModule,
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
