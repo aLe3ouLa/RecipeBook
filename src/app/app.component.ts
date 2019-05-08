c9a0b3ca62ccfc4ad99df2dd8a5f0f7fc4945d3e
aLe3ouLa <barka@csd.uoc.gr>
2019-05-08 21:20:14 +0300

    Sign up users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDS_thel3UmAcBA2feaOIl2q-dkPdQ70gQ",
+      authDomain: "ng-recipe-book-80661.firebaseapp.com",
+    });
+  }
 }
